f3ec38c38c2ffc05b63e2222f499b82db1a1dd30
Bruno79284 <suzanosalitao@gmail.com>
2021-10-13 16:28:36 -0300

    first commit

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,24 @@
+import 'package:cadastro_pessoa/models/user.dart';
+
+const DUMMY_USERS = {
+  '1', const User(
+  name: 'Maria',
+  email: 'maria@email.com',
+  avatarUrl: 'https://pixabay.com/pt/illustrations/pessoas-homem-mulheres-av%c3%b3-vov%c3%b4-4035403/',
+  ),
+  '2', const User(
+  name: 'Bruno',
+  email: 'bruno@email.com',
+  avatarUrl: 'https://pixabay.com/pt/vectors/menino-avatar-%c3%adcone-design-simples-1837375/',
+  ),
+  '3', const User(
+  name: 'Aline',
+  email: 'aline@email.com',
+  avatarUrl: 'https://pixabay.com/pt/illustrations/menina-cabe%c3%a7as-avatar-de-menina-4268927/',
+  ),
+  '4', const User(
+  name: 'Ronald',
+  email: 'ronald@email.com',
+  avatarUrl: 'https://pixabay.com/pt/illustrations/homem-artes-marciais-avatar-1602633/',
+  ),
+};
